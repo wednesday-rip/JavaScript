59b52a6bee3346a8d3eb881a51ef9dca83727065
Wagner Prates <wednesday@MacBook-Pro-de-Wagner.local>
2022-12-02 21:27:53 -0300

    First steps in JavaScript

---
+++ Day_1/index.js
@@ -0,0 +1,16 @@
+console.log("JavaScript is running!!!")
+
+//don't use (var) use (let) for creating variables.
+let nickName = "Wednesday"
+console.log(nickName)
+
+//To create a variable that cannot be changed, use (const) not (let).
+const price =30
+console.log(price)
+
+let itemName = "Pen" // string
+let itemPrice =  3 // number
+let itemAvailable =  true // true and false are boolean
+let itemColor  // When the variable is not defined, it is an undefined status.
+itemName = null // should use (null) when you want to clear the content stored in the variable
+
